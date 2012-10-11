0400cffda4e5c0339b2687a3982c9345cd651889
Munkhtsogt Tsogbadrakh <munkhuu48@gmail.com>
2012-10-11 16:42:28 +0800

    project initiliazation

---
+++ app/views.py
@@ -0,0 +1,41 @@
+"""
+Flask Module Docs:  http://flask.pocoo.org/docs/api/#flask.Module
+
+This file is used for both the routing and logic of your
+application.
+"""
+
+from google.appengine.api import mail
+from google.appengine.api import users
+from flask import Blueprint, url_for, render_template, request, redirect
+from models import Todo
+from forms import TodoForm, EmailForm
+
+views = Blueprint('views', __name__)
+
+
+@views.route('/')
+def index():
+    """Render website's index page."""
+    
+    user = users.get_current_user()
+    logout_url, login_url = None, None
+    
+    if user:
+        logout_url = users.create_logout_url("/")
+    else:
+        login_url = users.create_login_url("/")
+    return render_template('index.html', login_url=login_url, user=user, logout_url=logout_url)
+
+
+@views.after_request
+def add_header(response):
+    """Add header to force latest IE rendering engine and Chrome Frame."""
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    return response
+
+
+@views.app_errorhandler(404)
+def page_not_found(error):
+    """Custom 404 page."""
+    return render_template('404.html'), 404
